cf9589a53b589bbed6fd300488ffc0803af295ce
sandipto729 <sandipto729@gmail.com>
2024-10-20 21:02:50 +0530

    change to pdf format

--- backend/controller/User/UserAdmissionSignUp.js
+++ backend/controller/User/UserAdmissionSignUp.js
@@ -1,17 +1,21 @@
-const UserAdmissionModel=require('../../model/User/UserAdmissionModel');
+const UserAdmissionModel = require('../../model/User/UserAdmissionModel');
+const createUserPDF = require('../../helper/PdfJenerator');
 
-const userAdmissionSignUp=async(req,res)=>{
-    try{
-        const {email}=req.body;
-        const user=await UserAdmissionModel.findOne({email:email});
-        if(user){
-            return res.status(400).json({success:false,message:'User already exists'});
+const userAdmissionSignUp = async (req, res) => {
+    try {
+        const { email } = req.body;
+        const user = await UserAdmissionModel.findOne({ email: email });
+        if (user) {
+            return res.status(400).json({ success: false, message: 'User already exists' });
         }
-        const userAdmission=await UserAdmissionModel.create(req.body);
-        res.status(200).json({success:true,userAdmission});
-    }catch(err){
-        res.status(400).json({success:false,error:err.message});
+        const userAdmission = await UserAdmissionModel.create(req.body);
+        const pdfPath = await createUserPDF(userAdmission);
+
+        res.download(pdfPath, `${user.name}_details.pdf`);
+        
+    } catch (err) {
+        res.status(400).json({ success: false, error: err.message });
     }
 }
 
-module.exports=userAdmissionSignUp
+module.exports = userAdmissionSignUp
