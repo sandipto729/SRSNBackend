a1f614079131dda178ed07519895516d2b657f33
sandipto729 <sandipto729@gmail.com>
2024-12-02 21:01:43 +0530

    add redis in news,alumni and techer fetch

--- backend/controller/Notice/Noticefetch.js
+++ backend/controller/Notice/Noticefetch.js
@@ -1,9 +1,17 @@
 const NewsModel=require('../../model/Notice/NoticeModel');
+const RedisClient = require('../../config/Redis');
+const DEFAULT_EXPIRATION = 60 * 60 * 3;
 
 const NewsFetch = async (req, res) => {
     try {
+        const catchedNews = await RedisClient.get('news');
+        if (catchedNews) {
+            console.log('Using Redis cache getting news',JSON.parse(catchedNews));
+            return res.json({ success: true, notice: JSON.parse(catchedNews) });
+        }
         const notice = await NewsModel.find();
         res.status(200).json({ success: true, notice });
+        await RedisClient.set('news', JSON.stringify(notice), 'EX', DEFAULT_EXPIRATION);
     } catch (err) {
         console.error("Error fetching news:", err); // Log error details
         res.status(400).json({ success: false, error: err.message });
